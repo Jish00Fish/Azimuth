16c25da1304f9b4a0f734b70fb219aa2373cca4c
Joshua Miller <s220946@students.aie.edu.au>
2023-02-24 13:52:03 +1100

    poor framework compleate

---
+++ Azimuth/GameObjects/GameObjectManager.cs
@@ -0,0 +1,37 @@
+﻿namespace Azimuth.GameObjects
+{
+	public static class GameObjectManager  // gameObject and GameObjects swapped
+	{
+		private static List<GameObjects> gameObject = new List<GameObjects>();
+
+		public static void Spawn(GameObjects _gameObjects)
+		{
+			if(!gameObject.Contains(_gameObjects))
+			{
+				gameObject.Add(_gameObjects);
+				_gameObjects.Load();
+			}
+		}
+
+		public static void Destroy(GameObjects _gameObjects)
+		{
+			if(gameObject.Contains(_gameObjects))
+			{
+				_gameObjects.Unload();
+				gameObject.Remove(_gameObjects);
+			}
+		}
+
+		public static void Update(float _deltaTime)
+		{
+			foreach(GameObjects gameObjects in gameObject)
+				gameObjects.Update(_deltaTime);
+		}
+
+		public static void Draw()
+		{
+			foreach(GameObjects gameObjects in gameObject)
+				gameObjects.Draw();
+		}
+	}
+}
---
+++ Azimuth/GameStates/GameStateManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Azimuth.GameStates
+{
+	public class GameStateManager
+	{
+		// IGameStates has an s, IGameStates has an s
+		private static Dictionary<string, IGameStates> states = new Dictionary<string, IGameStates>();
+		private static List<IGameStates> activeStates = new List<IGameStates>();
+
+		private static List<Action> activateActions = new List<Action>();
+		private static List<Action> deactivateActions = new List<Action>();
+
+		public static void AddState(IGameStates _state)
+		{
+			if(!states.ContainsKey(_state.ID))
+				states.Add(_state.ID, _state);
+		}
+
+		public static void RemoveState(IGameStates _state)
+		{
+			if(states.ContainsKey(_state.ID))
+				states.Remove(_state.ID);
+		}
+
+		public static void Update(float _deltaTime)
+		{
+			foreach(Action action in deactivateActions)
+				action?.Invoke();
+					
+			foreach(Action action in activateActions)
+				action?.Invoke();
+			
+			deactivateActions.Clear();
+			activateActions.Clear();
+
+			foreach(IGameStates state in activeStates)
+			{
+				state.Update(_deltaTime);
+			}
+		}
+
+		public static void Draw()
+		{
+			foreach(IGameStates state in activeStates)
+			{
+				state.Draw();
+			}
+		}
+
+		private static void ActiveState(string _id)
+		{
+			if(states.ContainsKey(_id))
+			{
+				activateActions.Add(() =>
+				{
+					activeStates.Add(states[_id]);
+					states[_id].Load();
+				});
+			}
+		}
+
+		public static void DeactivateState(string _id)
+		{
+			if(states.ContainsKey(_id) && activeStates.Contains(states[_id]))
+			{
+				deactivateActions.Add(() =>
+				{
+					activeStates.Remove(states[_id]);
+					states[_id].Unload();
+				});
+			}
+		}
+		
+	}
+}
